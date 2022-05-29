616e685b9ac87c94f971a25910b95dba12a642be
andred2909 <andrea.dangelo@edu.unito.it>
2022-05-29 18:55:18 +0200

    UI dell'elenco servizi migliorato

--- lib/screens/servizi_elenco.dart
+++ lib/screens/servizi_elenco.dart
@@ -7,7 +7,7 @@ class ServiziElenco extends StatefulWidget {
 
 class _ServiziElencoState extends State<ServiziElenco> {
   int _selectedIndex = 0;
-  String dropdownvalue = 'Cucina';
+  String dropdownvalue = 'Informatica';
   var items = [
     'Informatica',
     'Cucina',
@@ -22,40 +22,56 @@ class _ServiziElencoState extends State<ServiziElenco> {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text("Servizi",
-                style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 0.5),
-              ),
-            ],
-          ),
-              Row(
-                children: <Widget>[
-                  DropdownButton(
-                    value: dropdownvalue,
-                    icon: const Icon(Icons.keyboard_arrow_down),
-                    items: items.map((String items) {
-                      return DropdownMenuItem(
-                        value: items,
-                        child: Text(items),
-                      );
-                    }).toList(),
-                    // After selecting the desired option,it will
-                    // change button value to selected value
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        dropdownvalue = newValue!;
-                      });
-                    },
+          Padding(padding: EdgeInsets.only(top: 60)),
+          Container(
+            child:
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                DefaultTextStyle(
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
                   ),
-                ],
-              ),
+                  child: Text("Servizi"),
+                ),
+              ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 20)),
+          Container(
+            child:
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                DropdownButton(
+                  value: dropdownvalue,
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  items: items.map((String items) {
+                    return DropdownMenuItem(
+                      value: items,
+                      child: Center(child: DefaultTextStyle(style: TextStyle(fontStyle: FontStyle.italic, color: Colors.black, fontSize: 18), child: Text(items))),
+                    );
+                  }).toList(),
+                  // After selecting the desired option,it will
+                  // change button value to selected value
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownvalue = newValue!;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 20)),
           Row(
             children: <Widget>[
               Expanded(
                 child: Column(
                   children: <Widget>[
-                    Text("Programmazione"),
+                    Center(child: DefaultTextStyle(style: TextStyle(color: Colors.black, fontSize: 18), child: Text('Programmazione'))),
                     Divider(
                       color: Colors.black,
                     ),
@@ -64,31 +80,55 @@ class _ServiziElencoState extends State<ServiziElenco> {
               ),
             ],
           ),
-          Flexible( //TODO: Cambiare in lista infinita
-            child: ListView(
-              shrinkWrap: true,
-              children: <Widget>[
-                ListTile(
-                  leading: Image.asset('assets/img/handshake.png'),
-                  title: Text('Map'),
-                ),
-                ListTile(
-                  leading: Image.asset('assets/img/handshake.png'),
-                  title: Text('Map'),
-                ),
-                ListTile(
-                  leading: Image.asset('assets/img/handshake.png'),
-                  title: Text('Map'),
-                ),
-                ListTile(
-                  leading: Image.asset('assets/img/handshake.png'),
-                  title: Text('Map'),
-                ),
-                ListTile(
-                  leading: Image.asset('assets/img/handshake.png'),
-                  title: Text('Map'),
-                ),
-              ],
+          Container(
+            child: Flexible( //TODO: Cambiare in lista infinita
+              child: ListView(
+                shrinkWrap: true,
+                children: const <Widget>[
+                  ListTile(
+                    dense: true,
+                    leading: Icon(Icons.person, size: 35),
+                    title: Center(child: DefaultTextStyle(style: TextStyle(color: Colors.black, fontSize: 20), child: Text("D'Angelo Andrea"))),
+                    subtitle: Center(child: DefaultTextStyle(style: TextStyle(fontStyle: FontStyle.italic, color: Colors.black, fontSize: 15), child: Text('C++'))),
+                  ),
+                  ListTile(
+                    dense: true,
+                    leading: Icon(Icons.person, size: 35),
+                    title: Center(child: DefaultTextStyle(style: TextStyle(color: Colors.black, fontSize: 20), child: Text("Lorenzo Dentis"))),
+                    subtitle: Center(child: DefaultTextStyle(style: TextStyle(fontStyle: FontStyle.italic, color: Colors.black, fontSize: 15), child: Text('Unity, JavaScript'))),
+                  ),
+                  ListTile(
+                    dense: true,
+                    leading: Icon(Icons.person, size: 35),
+                    title: Center(child: DefaultTextStyle(style: TextStyle(color: Colors.black, fontSize: 20), child: Text("Francesca Rinaldi"))),
+                    subtitle: Center(child: DefaultTextStyle(style: TextStyle(fontStyle: FontStyle.italic, color: Colors.black, fontSize: 15), child: Text('Java'))),
+                  ),
+                  ListTile(
+                    dense: true,
+                    leading: Icon(Icons.person, size: 35),
+                    title: Center(child: DefaultTextStyle(style: TextStyle(color: Colors.black, fontSize: 20), child: Text("Marta Meroni"))),
+                    subtitle: Center(child: DefaultTextStyle(style: TextStyle(fontStyle: FontStyle.italic, color: Colors.black, fontSize: 15), child: Text('HTML/CSS'))),
+                  ),
+                  ListTile(
+                    dense: true,
+                    leading: Icon(Icons.person, size: 35),
+                    title: Center(child: DefaultTextStyle(style: TextStyle(color: Colors.black, fontSize: 20), child: Text("Matteo Filisti"))),
+                    subtitle: Center(child: DefaultTextStyle(style: TextStyle(fontStyle: FontStyle.italic, color: Colors.black, fontSize: 15), child: Text('Flutter'))),
+                  ),
+                  ListTile(
+                    dense: true,
+                    leading: Icon(Icons.person, size: 35),
+                    title: Center(child: DefaultTextStyle(style: TextStyle(color: Colors.black, fontSize: 20), child: Text("Sandra Keyhole"))),
+                    subtitle: Center(child: DefaultTextStyle(style: TextStyle(fontStyle: FontStyle.italic, color: Colors.black, fontSize: 15), child: Text('React, UnReal Engine'))),
+                  ),
+                  ListTile(
+                    dense: true,
+                    leading: Icon(Icons.person, size: 35),
+                    title: Center(child: DefaultTextStyle(style: TextStyle(color: Colors.black, fontSize: 20), child: Text("Marco Demasi"))),
+                    subtitle: Center(child: DefaultTextStyle(style: TextStyle(fontStyle: FontStyle.italic, color: Colors.black, fontSize: 15), child: Text('Vue.js'))),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
