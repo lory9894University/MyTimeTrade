ba5cc6739e690bcafdcce5e68c1bb86cef0fe9aa
andred2909 <andrea.dangelo@edu.unito.it>
2022-06-30 17:18:17 +0200

    Aggiustata bottom bar, ma non su Accetta.dart

--- lib/screens/accetta.dart
+++ lib/screens/accetta.dart
@@ -524,60 +524,35 @@ class _AccettaState extends State<Accetta> {
               ],
           ),
           ),
+            //bottomNavigationBar: BottomBar(index: index, context: context),
             bottomNavigationBar: CurvedNavigationBar(
               index: index,
               backgroundColor: Colors.transparent,
               height: 60,
-              items: <Widget>[
-                IconButton(
-                  iconSize: 30,
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/amici');
-                  },
-                  icon: const Icon(Icons.handshake),
-                  tooltip: 'Invita i tuoi amici',
-                ),
-                IconButton(
-                  iconSize: 30,
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/accetta');
-                  },
-                  icon: const Icon(Icons.check_outlined),
-                  tooltip: 'Accetta',
-                ),
-                IconButton(
-                  iconSize: 30,
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/');
-                  },
-                  icon: const Icon(Icons.home),
-                  tooltip: 'Home',
-                ),
-                IconButton(
-                  iconSize: 30,
-                  onPressed: () {
-                    Navigator.pushNamed(
-                      context,
-                      '/profile',
-                      arguments: Profile_Passage(
-                        "Andrea",
-                        "Developer",
-                        "",
-                        "",
-                      ),
-                    );
-                  },
-                  icon: const Icon(Icons.person),
-                  tooltip: 'Profilo',
-                ),
-                IconButton(
-                  iconSize: 30,
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/servizi');
-                  },
-                  icon: const Icon(Icons.map),
-                  tooltip: 'Servizi',
-                ),
+              onTap: (index) {
+                if(index == 0) {
+                  Navigator.pushNamed(context, '/amici');
+                }
+                else if (index == 1) {
+                  Navigator.pushNamed(context, '/accetta');
+                }
+                else if (index == 2) {
+                  Navigator.pushNamed(context, '/');
+                }
+                else if (index == 3) {
+                  Navigator.pushNamed(context, '/profile',
+                      arguments: Profile_Passage("Andrea", "D'Angelo", "", ""));
+                }
+                else if (index == 4) {
+                  Navigator.pushNamed(context, '/servizi');
+                }
+              },
+              items: const <Widget>[
+                Icon(Icons.handshake, size: 30),
+                Icon(Icons.check_outlined, size: 30),
+                Icon(Icons.home, size: 30),
+                Icon(Icons.person, size: 30),
+                Icon(Icons.map, size: 30)
               ],
             )
         ),
