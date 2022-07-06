c34fe4c9484a54e84023bbbb07abd5a8e5976755
lory9894 <lory9894@gmail.com>
2022-07-06 14:09:20 +0200

    mostra solo transazioni completate

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -28,23 +28,26 @@ class _HomePageState extends State<HomePage> {
 
     DatabaseReference userRef =
         FirebaseDatabase.instance.ref('users/${user.uid}');
-    userRef.onValue.listen((DatabaseEvent event) {
-      pageData = event.snapshot.value as Map<dynamic, dynamic>;
+    DatabaseEvent event = await userRef.once();
+    pageData = event.snapshot.value as Map<dynamic, dynamic>;
 
-      if (pageData["transactions"] != null) {
-        List<Object?> trans_id = pageData["transactions"] as List<Object?>;
-        transactions.clear();
-        trans_id.forEach((value) {
-          DatabaseReference transaction_ref =
-              FirebaseDatabase.instance.ref('transaction/${value}');
-          transaction_ref.onValue.listen((DatabaseEvent event) {
-            transactions.add(event.snapshot.value);
+    if (pageData["transactions"] != null) {
+      List<Object?> trans_id = pageData["transactions"] as List<Object?>;
+      transactions.clear();
+      trans_id.forEach((value) {
+        DatabaseReference transaction_ref =
+            FirebaseDatabase.instance.ref('transaction/${value}');
+        transaction_ref.onValue.listen((DatabaseEvent event) {
+          var transaction = event.snapshot.value as Map<dynamic, dynamic>;
+          if (transaction['status'] == 'completed') {
+            transactions.add(transaction);
             setState(() => {});
-          });
+          }
         });
-      }
-      setState(() => {});
-    });
+      });
+    }
+
+    setState(() => {});
 
     //Query transactions_query = ;
   }
@@ -127,7 +130,7 @@ class _HomePageState extends State<HomePage> {
                                 color: Colors.black,
                                 fontSize: 20),
                             child: Text(
-                                'data: ${transactions[index]["date"]}, importo: ${transactions[index]["amount"]}  '))),
+                                'data: ${transactions[index]["date"]}, importo: ${transactions[index]["amount"]} \n lavoro svolto: ${transactions[index]["description"]}'))),
                   );
                 },
                 shrinkWrap: true,
