3378f51023e41558c401631cb9857e337b7d9515
lory9894 <lory9894@gmail.com>
2022-08-27 13:25:16 +0200

    hompage testing in pausa (devo mergare)

--- test/homepage_test.dart
+++ test/homepage_test.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,10 +9,17 @@ import 'package:mytimetrade/screens/home_page.dart';
 
 import 'test_helper.dart';
 
-void main() {
+Future<void> main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
   setupFirebaseAuthMocks();
-  Firebase.initializeApp();
+  await Firebase.initializeApp();
+
+  FirebaseFirestore.instance.settings = const Settings(
+      host: 'localhost:8080', sslEnabled: false, persistenceEnabled: false);
+  FirebaseDatabase database = FirebaseDatabase(
+    app: Firebase.app(),
+    databaseURL: "localhost:9000",
+  );
 
   testWidgets('transaction rendering', (WidgetTester tester) async {
     FlutterError.onError = ignoreOverflowErrors;
